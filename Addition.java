ba146362100ad19dc4d72798ef1e9bcecdf35f54
Lijokesavan <135047389+Lijokesavan@users.noreply.github.com>
2023-10-31 14:31:10 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,29 @@
+package com.simple.execise;
+
+class Main{
+public class Addition { 
+	public static void main(String[] args) {
+		
+		//declare variables and assign values
+		
+		int num1 = 10;
+		float num2=12;
+		char character = 'A';
+		boolean bool = true;
+		String str ="Great Learning";
+		double num3= 10.23;
+		
+		//display all the variable on the console
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(character);
+		System.out.println(bool);
+		System.out.println(str);
+		System.out.println(num3);
+	
+		
+
+	}
+
+}
+}
